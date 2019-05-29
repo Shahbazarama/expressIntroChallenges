2c2057988f955e0bf643934e144ebc56dcfe7e3e
Raffi Shahbazian <raffi.p.shahbazian@gmail.com>
2019-05-29 13:06:58 -0700

    attempting part 2

--- part2/server.js
+++ part2/server.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000;
+var path = require('path');
+var fs = require('fs');
 
-app.get('/yourroute', function(req, res) {
-  res.send("stuff");
+app.post('/create/:name/:age', function(req, res) {
+
+  let personObject = {
+    "name": req.params.name,
+    "age" : req.params.age
+  }
+  let storage = fs.readFileSync(path.join(__dirname + '/storage.json')).toString()
+  storage += JSON.stringify(personObject)
+  fs.writeFileSync(path.join(__dirname + '/storage.json'), storage)
+  res.send("good")
+});
+
+app.get('/', function(req, res) {
+  res.send(fs.readFileSync(path.join(__dirname + '/storage.json')).toString());
 });
 
 app.use(function(req, res) {
