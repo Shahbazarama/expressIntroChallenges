a95cb8dcb9ff072438166dafc93da5f2c2438c48
Raffi Shahbazian <raffi.p.shahbazian@gmail.com>
2019-05-29 11:27:11 -0700

    challenges 1-3 complete

--- part1/server.js
+++ part1/server.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000;
+var path = require('path');
 
-app.get('/yourroute', function(req, res) {
-  res.send("stuff");
+app.get('/hello', function(req, res) {
+  res.send("Hello!");
+});
+
+app.post('/create/:name', function(req, res) {
+  let someJSON = {
+    "id": 1,
+    "name": req.params.name
+  }
+  res.json(someJSON);
+});
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.use(function(req, res) {
